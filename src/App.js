585d812d56a1f59821dd6c286784b8d78a58c124
changhyun-Hong <alex963@naver.com>
2023-08-31 22:20:47 +0900

    feat: upload

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Login from './pages/Login';
 
 function App() {
     const { chattingRoom_list } = useChattingRoom();
-    const [isLogined, setIsLogined] = useState(true);
+    const [isLogined, setIsLogined] = useState(false);
 
     if (!isLogined) {
         return (
